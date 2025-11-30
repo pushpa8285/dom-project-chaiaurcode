3e793ea963bc230050a85d13461f7b8a74500f9f
Pushpa <pushpa82855@gmail.com>
2025-11-30 09:32:07 +0530

    xcx

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -1,2 +1,20 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector("body")
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(){
+        console.log(button.id)
+        if (button.id === "grey"){
+            body.style.backgroundColor = button.id
+        }
+        if (button.id === "white"){
+            body.style.backgroundColor = button.id
+        }
+        if (button.id === "blue"){
+            body.style.backgroundColor = button.id
+        }
+        if (button.id === "yellow"){
+            body.style.backgroundColor = button.id
+        }
+    })
+})
