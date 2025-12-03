51e592b653981cff0f3ad4d3c74b2800fc3afec1
pushpa <pushpa82855@gmail.com>
2025-12-03 06:46:01 +0530

    project-4

---
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -0,0 +1,43 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector("#subt")
+const guessField = document.querySelector("#guessField");
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const resultParas = document.querySelector(".resultParas");
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let nkumGuess = 1;
+
+let playGame = true;
+
+if(playGame){
+    submit
+}
+
+function validateGuess(guess){
+    //
+}
+
+function checkGuess(guess){
+    //
+}
+
+function displayGuess(guess){
+    //
+}
+
+function displayMessage(guess){
+    //
+}
+
+function endGame(guess) {
+  //
+}
+
+function newGame(guess){
+    //
+}
